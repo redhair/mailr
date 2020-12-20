373649e14f2860241d8c3bb403cafe51875b5eb0
redhair <thomasbonanni@afito.com>
2020-12-20 16:22:41 -0500

    [fix] can now subscribe to multiple mailing lists with same email

--- pages/api/users/[id]/index.js
+++ pages/api/users/[id]/index.js
@@ -63,7 +63,7 @@ export default mongoMiddleware(async (req, res, connection, models) => {
     },
     PUT: (response) => {
       models.User.update(
-        { _id: id, 'subscribers.email': { $ne: body.subscriber.email } },
+        { _id: id },
         {
           $push: {
             subscribers: {
